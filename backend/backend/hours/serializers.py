efb218225e30885aeade52788b2f613590ba314e
Rodas Jateno <se.rodas.workicho@gmail.com>
2020-11-07 20:38:41 +0300

    Added One Field for Testing

---
+++ backend/backend/hours/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Hour
+from rest_framework import viewsets
+
+class HourSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hour
+        fields = ('id', 'name')
